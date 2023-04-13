8d6244ba46ba34db1afb45b2c4ec563391c7f26f
Ishii-Nozomi <xx2468.egg@gmail.com>
2023-04-14 01:58:50 +0900

    セル追加ボタンを追加

---
+++ 領収shoppingApp/AddButtonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddButtonTableViewCell.swift
+//  領収shoppingApp
+//
+//  Created by NOZOMI ISHII on 2023/04/14.
+//
+
+import UIKit
+
+class AddButtonTableViewCell: UITableViewCell {
+    @IBOutlet weak var addCellButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
